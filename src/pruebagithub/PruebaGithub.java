80f26c97973b74ff2750bd172d5182dbf1e71a76
dirgo <dirgo@MSI>
2023-05-10 17:53:08 +0200

    Tercera subida

--- src/pruebagithub/PruebaGithub.java
+++ src/pruebagithub/PruebaGithub.java
@@ -14,7 +14,7 @@ public class PruebaGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Github, qué tal?");
+        System.out.println("Hola Github, qué talasdasdasdasd?");
     }
     
 }
