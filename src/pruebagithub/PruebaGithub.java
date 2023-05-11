ee05d70cb0d56f1db056de76940111616c6a2813
dirgo <dirgo@MSI>
2023-05-11 18:07:23 +0200

    Hola

--- src/pruebagithub/PruebaGithub.java
+++ src/pruebagithub/PruebaGithub.java
@@ -14,7 +14,7 @@ public class PruebaGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Github, qué talasdasdasdasd?");
+        System.out.println("Hola Github, qué taladasd?");
     }
     
 }
